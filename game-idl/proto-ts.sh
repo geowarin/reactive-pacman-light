79436fa9455e7cf47af5f6f276d54cc665c653b8
Geoffroy Warin <code@geowarin.com>
2019-12-30 06:20:08 +0100

    Refactor generation

--- game-idl/proto-ts.sh
+++ game-idl/proto-ts.sh
@@ -1,16 +1,22 @@
+set -e
+
+rm -rf ./generated/javascript
 mkdir -p ./generated/javascript
 
+proto_dir="./src/main/proto"
+
 protoc --proto_path=node_modules/rsocket-rpc-protobuf/proto \
-  --proto_path=../proto \
-  --js_out=import_style=commonjs,binary:./generated/javascript \
+  --proto_path="$proto_dir"\
   --rsocket_rpc_out=./generated/javascript \
   --plugin=protoc-gen-rsocket_rpc=node_modules/.bin/rsocket_rpc_js_protoc_plugin \
-  ../proto/*.proto \
-  node_modules/rsocket-rpc-protobuf/proto/rsocket/*.proto
+  --js_out=import_style=commonjs,binary:./generated/javascript \
+  node_modules/rsocket-rpc-protobuf/proto/rsocket/*.proto \
+  "$proto_dir"/*.proto
 
-protoc --proto_path=../proto --proto_path=./node_modules/rsocket-rpc-protobuf/proto \
+protoc --proto_path="$proto_dir" \
+  --proto_path=./node_modules/rsocket-rpc-protobuf/proto \
   --proto_path=./node_modules/rsocket-rpc-protobuf/proto/rsocket \
   --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts \
   --ts_out=./generated/javascript \
   ./node_modules/rsocket-rpc-protobuf/proto/rsocket/*.proto \
-  ../proto/*.proto
+  "$proto_dir"/*.proto
